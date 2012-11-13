327d48cadc25727f4d07808f94ac2dfba6c220da
Boris Dinkevich <Boris@dinkevich.com>
2012-11-13 12:33:25 +0200

    Added plugin settings screen

--- init.rb
+++ init.rb
@@ -9,4 +9,12 @@ Redmine::Plugin.register :practitest do
   description 'Integrate Practitest with Redmine issues'
   version '0.0.1'
   author_url 'http://www.practitest.com'
+
+  settings(:partial => 'settings/practitest_settings',
+           :default => {
+            'api_key'     => '',
+            'api_secret'  => '',
+            'project_id'  => '',
+            'url'         => 'https://prod.practitest.com'
+          })
 end
